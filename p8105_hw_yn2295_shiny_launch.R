6055ad8e483564edab82c4b306a450f43850fdff
Yanelli nunez <yn22@cumc.columbia.edu>
2017-11-16 16:48:55 -0500

    Removed shiny files and add a dashboard link to my website

--- p8105_hw_yn2295_shiny_launch.R
+++
@@ -1,8 +0,0 @@
-rsconnect::setAccountInfo(name = 'lizzyagibson',
-                          token = '4C13735CFABFE8F66E36D5699E5035AE',
-                          secret = 'fOIXYh9NEQDoiI0O69cQiNiX9JvoKH17OfR0DOqu')
-
-rsconnect::deployApp('./p8105_hw_yn2295_shiny.Rmd')
-
-
-
